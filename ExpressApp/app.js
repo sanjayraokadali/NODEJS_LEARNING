6038b734e31fdb17b9ffbd5b30daf92ca6553046
Sanjay Kadali <sr.kadali@outlook.com>
2024-09-05 23:20:43 +0530

    Filtering Routes

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -8,9 +8,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false})); // a middleware function for parsing body
 
-app.use(adminRoutes);
-
+app.use('/admin', adminRoutes); // Adding Route Filters.
 app.use(shopRoutes);
+
+app.use((req, res, next) =>{ // Page Not Found Handler!
+    res.status(404).send('<h2>Page Not Found</h2>');
+});
 
 
 app.listen(3001);
--- ExpressApp/routes/admin.js
+++ ExpressApp/routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) =>{
+router.get('/add-product', (req, res, next) =>{ // TAKES IN A GET REQUEST
 
     console.log('/add-product');
 
-    res.send('<form action="/product" method="POST"> \
+    res.send('<form action="/admin/product" method="POST"> \
              \
              <input type="text" name="productname"> \
              <input type="submit" value="add product">\
@@ -15,12 +15,16 @@ router.get('/add-product', (req, res, next) =>{
 
 });
 
-router.post('/product', (req, res, next) =>{
+router.post('/product', (req, res, next) =>{ // TAKES IN A POST REQUEST
 
     console.log('/add-product');
     var value = req.body.productname;
     res.send('<h4>'+value+'</h4>')
     
+});
+
+router.get('/product', (req, res, next) => {
+    res.status(200).send("<h5>GET REQUEST FOR PRODUCT</h5>");
 });
 
 module.exports = router;
