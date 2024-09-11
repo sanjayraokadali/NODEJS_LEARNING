63e46fc369e1ae7ecf61453984e72b123c7f34db
Sanjay Kadali <sr.kadali@outlook.com>
2024-09-11 18:03:42 +0530

    Added registration page

--- iBank/routes/baseRoute.js
+++ iBank/routes/baseRoute.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 
-
+router.get('/register-user', (req,res)=>{
+    res.render('register-user.hbs')
+})
 
 router.get('/', (req, res)=>{
 
