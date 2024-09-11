76ed85ef2beb08c55fc031e9bb68ace3ea93978c
Sanjay Kadali <sr.kadali@outlook.com>
2024-09-11 19:01:01 +0530

    Added Login Page and Register Page

--- iBank/routes/baseRoute.js
+++ iBank/routes/baseRoute.js
@@ -7,6 +7,10 @@ router.get('/register-user', (req,res)=>{
     res.render('register-user.hbs')
 })
 
+router.get('/login', (req,res)=>{
+    res.render('login.hbs');
+});
+
 router.get('/', (req, res)=>{
 
     res.render('homepage.hbs');
